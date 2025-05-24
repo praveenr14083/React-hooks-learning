9970b8b5203d8ad4d7bc680a52c97cef47af2bb5
praveenr14083 <praveenr14083@gmail.com>
2025-05-24 20:49:26 +0530

    feat: useStete Method 2 implemented

--- src/UseState.jsx
+++ src/UseState.jsx
@@ -8,17 +8,42 @@ export default function UseState() {
 
   return (
     <div>
+      {/* Methood 1 */}
       {/* Display the current value of 'count' */}
-      <h1>{count}</h1>
+      {/* <h1>{count}</h1> */}
 
       {/* Button to increment the count by 1 when clicked */}
-      <button onClick={() => setCount(count + 1)}>Increment</button>
+      {/* <button onClick={() => setCount(count + 1)}>Increment</button> */}
 
-      <br />
-      <br />
+      {/* <br /> */}
+      {/* <br /> */}
 
       {/* Button to decrement the count by 1 when clicked */}
-      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      {/* <button onClick={() => setCount(count - 1)}>Decrement</button> */}
+
+      {/* Methood 2 */}
+      {/* Without using 'count' state variable */}
+      <h1>{count}</h1>
+      <button
+        onClick={() =>
+          setCount((preState) => {
+            return preState + 1;
+          })
+        }
+      >
+        Increment
+      </button>
+      <button
+        onClick={() =>
+          setCount((preState) => {
+            return preState - 1;
+          })
+        }
+      >
+        Decrement
+      </button>
+
+      <button onClick={() => setCount(0)}>Reset</button>
     </div>
   );
 }
